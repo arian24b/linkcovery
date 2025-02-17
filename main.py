831b0b1744c82ab7dc87ce28088eb506b80cd3b3
arian <arian24b@gmail.com>
2025-02-17 18:03:28 +0330

    Refactor CLI structure by consolidating initialization in main.py and removing unused files

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-from typer import Typer
-
-from app.cli.commands import user, link  # , import_export
-from app.core.settings import settings
-
-
-# Initialize Typer for potential future CLI enhancements
-cli_app = Typer(
-    name=settings.APP_NAME,
-    no_args_is_help=True,
-    help=f"{settings.APP_NAME}, Bookmark management CLI Application",
-)
-
-cli_app.add_typer(user.app, name="user")
-cli_app.add_typer(link.app, name="link")
-# cli_app.add_typer(import_export.app, name="import-export")
-
+from app.cli import cli_app
 
 if __name__ == "__main__":
     cli_app()
