160d7e5f79583970df1d78edb5b754042fc1a443
arian <arian24b@gmail.com>
2025-04-05 18:03:41 +0330

    Refactor update_link_description to improve logging and track updated links

--- app/cli/commands/script.py
+++ app/cli/commands/script.py
@@ -1,9 +1,8 @@
 from typer import Typer
 
-from app.core.logger import AppLogger
 from app.core.database import link_service
 from app.core.fetch_description import fetch_description
-
+from app.core.logger import AppLogger
 
 logger = AppLogger(__name__)
 app = Typer(no_args_is_help=True)
@@ -11,11 +10,13 @@ app = Typer(no_args_is_help=True)
 
 @app.command(help="Add a new link to the database.")
 def update_link_description() -> None:
+    updated_links = []
+
     for link in link_service.get_links():
         if link.description == "Imported from TXT" or not link.description:
             if description := fetch_description(link.url):
                 link_service.update_link(link.id, description=description)
-                logger.info(f"Link {link.id} updated with fetched description.")
+                updated_links.append(link.id)
             else:
                 logger.error(f"Failed to fetch description for link {link.id}.")
-    logger.info("All links processed.")
+    logger.info(f"Total links updated: {len(updated_links)}\n Links updated:\n{updated_links}")
