caff02529a68fb373b2ea84609a3277335b9a4b9
arian <arian24b@gmail.com>
2025-04-06 10:20:47 +0330

    Fix User-Agent string formatting and improve response handling in fetch_description

--- app/core/fetch_description.py
+++ app/core/fetch_description.py
@@ -7,7 +7,7 @@ from app.core.settings import settings
 
 def fetch_description(url: str) -> str:
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
         "Accept-Language": "en-US,en;q=0.5",
         "Accept-Encoding": "gzip, deflate, br",
         "Connection": "keep-alive",
@@ -18,15 +18,15 @@ def fetch_description(url: str) -> str:
         with Client(headers=headers, proxy=settings.PROXY, verify=False, follow_redirects=True) as client:
             response = client.get(url)
 
-        if response.is_success:
-            meta_description_pattern = compile(
-                r'<meta\s+name=["\']description["\']\s+content=["\'](.*?)["\']',
-                IGNORECASE,
-            )
+            if response.is_success:
+                meta_description_pattern = compile(
+                    r'<meta\s+name=["\']description["\']\s+content=["\'](.*?)["\']',
+                    IGNORECASE,
+                )
 
-            if match := meta_description_pattern.search(response.text):
-                return match.group(1)
-            return "No description tag found."
+                if match := meta_description_pattern.search(response.text):
+                    return match.group(1)
+                return "No description tag found."
         return f"Failed to fetch the page, status code: {response.status_code}"
 
     except RequestError as e:
